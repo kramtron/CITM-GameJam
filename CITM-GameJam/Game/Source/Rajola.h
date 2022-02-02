8422d0f4265d34dda737b5806fcc0ffe12bbaaf9
Biel Rabasa <bielrabasa@gmail.com>
2022-02-02 22:06:58 +0100

    DrawRajoles

--- CITM-GameJam/Game/Source/Rajola.h
+++ CITM-GameJam/Game/Source/Rajola.h
@@ -7,13 +7,13 @@
 
 class rajola {
 public:
-	rajola(fPoint screenPosition, iPoint spritePosition, float angle = 0.0f, float scale = 0.0f) {
+	rajola(iPoint screenPosition, iPoint spritePosition, float angle = 0.0f, float scale = 0.0f) {
 		p = screenPosition;
 		sp = spritePosition;
 		a = angle;
 		s = scale;
 		grab = false;
-		grabPosition = fPoint(0, 0);
+		grabPosition = iPoint(0, 0);
 		c = 30.0f * scale;	//30 = normal width height
 	}
 
@@ -21,13 +21,14 @@ public:
 
 	void setGrab(bool b);
 	bool Grabbed();
-private:
-	fPoint p;	//screen position
+
+public:
+	iPoint p;	//screen position
 	iPoint sp;	//sprite position
 	float a;	//angle
 	float s;	//scale
 	bool grab;
-	fPoint grabPosition;	//The coordinates in which the object is grabbed
+	iPoint grabPosition;	//The coordinates in which the object is grabbed
 	float c;	//collider width and height
 };
 #endif
